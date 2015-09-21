92c16a97884dde85335ffb55be3ba0762d8edfbe
M Starch <lestarch@googlemail.com>
2015-09-21 00:05:18 -0700

    lestarch: receiver

--- Makefile
+++ Makefile
@@ -1,14 +1,16 @@
 #libdata.dylib:	include/utils.h include/data.h src/utils.c src/data.c
 #	gcc -dynamiclib -o libdata.dylib -fPIC -I./include/ -dy src/utils.c src/data.c
-all: client server stream
+all: client server stream receiver
 
 stream:	include/utils.h include/data.h include/hyper-socket.h libdata.so src/stream.c
 	gcc -g -o stream -I./include/ -L. -ldata src/stream.c
 server:	include/utils.h include/data.h include/hyper-socket.h libdata.so src/server.c
 	gcc -o server -I./include/ -L. -ldata src/server.c
-client:	include/utils.h include/hyper-socket.h libdata.so src/client.c
+receiver:	include/utils.h include/hyper-socket.h libdata.so src/receiver.c
+	gcc -o receiver -I./include -L. -ldata src/receiver.c
+client:	include/utils.h include/hyper-socket.h libdata.so src/receiver.c
 	gcc -o client -I./include -L. -ldata src/client.c
 libdata.so:	include/utils.h include/data.h include/hyper-socket.h src/utils.c src/data.c src/hyper-socket.c
 	gcc -shared -o libdata.so -fPIC -I./include/ src/utils.c src/hyper-socket.c src/data.c
 clean:
-	rm -f libdata.so client server stream
+	rm -f libdata.so client server stream receiver
---
+++ src/receiver.c
@@ -0,0 +1,65 @@
+/*
+ * client.c
+ *
+ *  Created on: Jul 29, 2015
+ *      Author: lestarch
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+#include "hyper-socket.h"
+#include "utils.h"
+#include "data.h"
+
+//Globals for signal handling
+double start,end;
+int conn,list = -1;
+size_t total = 0;
+
+static void catch(int signo) {
+    printf("Total %zu bytes in %f seconds or %fB/s\n",total,(end-start),total/(end-start));
+    close(conn);
+    close(list);
+    exit(0);
+}
+
+/**
+ * Main program
+ */
+int main(int argc, char** argv) {
+    int r = signal(SIGINT, catch) == SIG_ERR;
+    fail(r,1,"Failed to register signal handler");
+    if (argc != 2)
+    {
+        printf("%s:\n\t<port>\n",argv[0]);
+        exit(-1);
+    }
+    short port = (short)atoi(argv[2]);
+    char buffer[BLOCK_SIZE];
+    printf("Establishing connection to: %d\n",port);
+    list = internet(port,htonl(INADDR_ANY),SERVER);
+    printf("Establishing connection to: %d\n",port);
+    conn = establish(list);
+    printf("Reading data from: %s:%d\n",argv[1],port);
+    int ret = 1;
+    start = now();
+
+    while (0 < ret) {
+        sigset_t set,old;
+        sigfillset(&set);
+        sigprocmask(SIG_BLOCK,&set,&old);
+        ret = read(conn,buffer,BLOCK_SIZE);
+        total += ret;
+        end = now();
+        sigprocmask(SIG_SETMASK,&old,NULL);
+    }
+
+    printf("Read %zu bytes in %f seconds or %fB/s\n",total,(end-start),total/(end-start));
+    close(conn);
+    close(list);
+}
